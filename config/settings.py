70291730d5be54eeec54993533ed58740324b560
futurevision108 <myfuturevision108@gmail.com>
2025-10-30 11:01:19 +0000

    use whitenoise for Heroku

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,9 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')   
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
