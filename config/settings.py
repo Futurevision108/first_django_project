ac64536ee06d558eace598d360c8f7822684648b
futurevision108 <myfuturevision108@gmail.com>
2025-10-31 23:43:51 +0000

    Post model built  and connected it to the admin dashboard

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -134,8 +139,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional directories to look for static files (e.g. a project-level "static/" folder)
+# Use string paths to avoid issues with some storage backends
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    str(BASE_DIR / 'static'),
 ]
 
 # Use WhiteNoise storage backend to serve compressed, cacheable static files in production
